3e48e57bfa2346c381e9bde3cf3e4c031219aa79
Miriam <miriamjoshua00@gmail.com>
2023-01-01 19:56:57 -0500

    commit tree ff985dad6131c780b3a6cc89bcf66b24ad199236

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk"
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import catsReducer from "./features/cats/catsSlice.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(catsReducer, applyMiddleware(thunkMiddleware))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById("root")
+);
